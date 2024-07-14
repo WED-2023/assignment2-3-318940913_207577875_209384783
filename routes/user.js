a0fc186c7ebdc8987026e33d9641b5a41bdf782b
Guy Biton <guybito@post.bgu.ac.il>
2024-07-14 16:08:52 +0300

    Server - Final Before Connection To Client

--- routes/user.js
+++ routes/user.js
@@ -9,12 +9,14 @@ const recipe_utils = require("./utils/recipes_utils");
  */
 router.use(async function (req, res, next) {
   if (req.session && req.session.user_id) {
-    DButils.execQuery("SELECT user_id FROM Users").then((users) => {
-      if (users.find((x) => x.user_id === req.session.user_id)) {
-        req.user_id = req.session.user_id;
-        next();
-      }
-    }).catch(err => next(err));
+    DButils.execQuery("SELECT user_id FROM Users")
+      .then((users) => {
+        if (users.find((x) => x.user_id === req.session.user_id)) {
+          req.user_id = req.session.user_id;
+          next();
+        }
+      })
+      .catch((err) => next(err));
   } else {
     res.sendStatus(401);
   }
@@ -23,71 +25,80 @@ router.use(async function (req, res, next) {
 /**
  * This path gets body with recipeId and save this recipe in the lastviewed list of the logged-in user
  */
-router.post('/LastViewedRecipes', async (req,res,next) => {
-  try{
-    if (!req.session.user_id) {throw { status: 401, message: "No User Logged in." };}
+router.post("/LastViewedRecipes", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
     const user_id = req.session.user_id;
     const recipe_id = req.body.recipeId;
-    await user_utils.markAsViewed(user_id,recipe_id);
+    await user_utils.markAsViewed(user_id, recipe_id);
     res.status(200).send("The Recipe successfully saved as LastViwed");
-    } 
-    catch(error)
-    {
+  } catch (error) {
     next(error);
-    }
-})
+  }
+});
 /**
  * This path returns the LastViewed recipes that were saved by the logged-in user
  */
-router.get('/LastViewedRecipes', async (req,res,next) => {
-  try{
-    if (!req.session.user_id) {throw { status: 401, message: "No User Logged in." };}
+router.get("/LastViewedRecipes", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
     const user_id = req.session.user_id;
     const recipes_id = await user_utils.getLastViewedRecipes(user_id);
     let recipes_id_array = [];
     recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
     const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
-  } catch(error){
-    next(error); 
+  } catch (error) {
+    next(error);
   }
 });
-
 
 /**
  * This path gets body with recipeId and save this recipe in the favorites list of the logged-in user
  */
-router.post('/FavoritesRecipes', async (req,res,next) => {
-  try{
-    if (!req.session.user_id) {throw { status: 401, message: "No User Logged in." };}
+router.post("/FavoritesRecipes", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
     const user_id = req.session.user_id;
     const recipe_id = req.body.recipeId;
-    await user_utils.markAsFavorite(user_id,recipe_id);
+    await user_utils.markAsFavorite(user_id, recipe_id);
     res.status(200).send("The Recipe successfully saved as favorite");
-    } catch(error){
+  } catch (error) {
     next(error);
   }
-})
+});
 
 /**
  * This path returns the favorites recipes that were saved by the logged-in user
  */
-router.get('/FavoritesRecipes', async (req,res,next) => {
-  try{
-    if (!req.session.user_id) {throw { status: 401, message: "No User Logged in." };}
+router.get("/FavoritesRecipes", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
     const user_id = req.session.user_id;
     const recipes_id = await user_utils.getFavoriteRecipes(user_id);
-    if(recipes_id.length == 0){throw { status: 203, message: "This user has no favorite Recipes ." };}
+    if (recipes_id.length == 0) {
+      throw { status: 203, message: "This user has no favorite Recipes ." };
+    }
     const results = await recipe_utils.getRecipesPreview(recipes_id);
     res.status(200).send(results);
-  } catch(error){
-    next(error); 
+  } catch (error) {
+    next(error);
   }
 });
 
-router.delete('/FavoritesRecipes', async (req, res, next) => {
+router.delete("/FavoritesRecipes", async (req, res, next) => {
   try {
-    if (!req.session.user_id) {throw { status: 401, message: "No User Logged in." };}
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
     const user_id = req.session.user_id;
     const recipe_id = req.body.recipeId;
     await user_utils.removeFavorite(user_id, recipe_id);
@@ -97,10 +108,8 @@ router.delete('/FavoritesRecipes', async (req, res, next) => {
   }
 });
 
-
 router.post("/addNewRecipe", async (req, res, next) => {
-  try
-  {
+  try {
     let recipe_details = {
       user_id: req.session.user_id,
       title: req.body.title,
@@ -112,10 +121,109 @@ router.post("/addNewRecipe", async (req, res, next) => {
       vegetarian: req.body.vegetarian,
       is_gluten_free: req.body.is_gluten_free,
       ingredients: req.body.ingredients,
-      instructions: req.body.instructions
-    }
+      instructions: req.body.instructions,
+    };
     recipe_id = await user_utils.addNewRecipe(recipe_details);
-    res.status(201).send({ message: "Recipe has been successfully created.", success: true });
+    res.status(201).send({
+      message: "Recipe has been successfully created.",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/MyMeal", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
+    const user_id = req.session.user_id;
+    const recipes_info = await user_utils.getMyMealRecipes(user_id);
+    const recipes_id = recipes_info.map((recipe) => {
+      return recipe.recipe_id;
+    });
+    if (recipes_id.length == 0) {
+      throw { status: 203, message: "This user has no recipes in his meal." };
+    }
+    // Get the recipe previews
+    const recipePreviews = await recipe_utils.getRecipesPreview(recipes_id);
+    // Merge recipe progress into the recipe previews
+    const results = await user_utils.fetchRecipeProgress(
+      recipes_info,
+      recipePreviews
+    );
+    res.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/MyMeal", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    await user_utils.addToMyMeal(user_id, recipe_id);
+    res.status(200).send("The Recipe successfully add to user meal.");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/MyMeal", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    await user_utils.removeFromMyMeal(user_id, recipe_id);
+    res.status(200).send("The Recipe successfully removed from user meal");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/RecipeMaking", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    // Checks is recipe_id is located in my meal, if not it will throw an error
+    const recipe_info = await user_utils.getMyMealRecipes(user_id, recipe_id);
+    const recipePreviews = await recipe_utils.getRecipesPreview([recipe_id]);
+    // Merge recipe progress into the recipe previews
+    const results = await user_utils.fetchRecipeProgress(
+      recipe_info,
+      recipePreviews
+    );
+    res.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/RecipeMaking", async (req, res, next) => {
+  try {
+    if (!req.session.user_id) {
+      throw { status: 401, message: "No User Logged in." };
+    }
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    const recipe_progress = "[" + req.body.recipe_progress.toString() + "]";
+    await user_utils.updateRecipeProgressInMyMeal(
+      user_id,
+      recipe_id,
+      recipe_progress
+    );
+    res
+      .status(200)
+      .send("You have successfully update the recipe making progress.");
   } catch (error) {
     next(error);
   }
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -4,201 +4,230 @@ const api_domain = "https://api.spoonacular.com/recipes";
 
 /**
  * Get recipes list from spooncular response and extract the relevant recipe data for preview
- * @param {*} recipes_info 
+ * @param {*} recipes_info
  */
 
 async function getRandomRecipes(number = 3) {
-    const params = {
-        apiKey: process.env.spooncular_apiKey,
-        number: number
+  const params = {
+    apiKey: process.env.spooncular_apiKey,
+    number: number,
+  };
+  const url = `${api_domain}/random`;
+  const randomRecipes = await axios.get(url, { params });
+  // return response.data.recipes;
+  if (randomRecipes.data.recipes.length > 0) {
+    return randomRecipes.data.recipes.map((recipe) => ({
+      id: recipe.id,
+      title: recipe.title,
+      readyInMinutes: recipe.readyInMinutes,
+      image: recipe.image,
+      aggregateLikes: recipe.aggregateLikes,
+      vegan: recipe.vegan,
+      vegetarian: recipe.vegetarian,
+      glutenFree: recipe.glutenFree,
+    }));
+  } else {
+    throw {
+      status: 500,
+      message: "No random recipes found or issue with Spoonacular API.",
     };
-    const url = `${api_domain}/random`;  
-    const randomRecipes = await axios.get(url, { params }); 
-    // return response.data.recipes;
-    if (randomRecipes.data.recipes.length > 0) {
-        return randomRecipes.data.recipes.map(recipe => ({
-            id: recipe.id,
-            title: recipe.title,
-            readyInMinutes: recipe.readyInMinutes,
-            image: recipe.image,
-            aggregateLikes: recipe.aggregateLikes,
-            vegan: recipe.vegan,
-            vegetarian: recipe.vegetarian,
-            glutenFree: recipe.glutenFree
-          }));
-        }
-    else{
-        throw { status: 500, message: "No random recipes found or issue with Spoonacular API." };
+  }
+}
+
+async function getRecipeInformation(recipe_id, is_search = false) {
+  const checkIfFromDB = await DButils.execQuery(
+    `SELECT 1 FROM MyRecipes WHERE recipe_id = '${recipe_id}'`
+  );
+
+  if (checkIfFromDB.length > 0 && !is_search) {
+    const recipeInformataion = (
+      await DButils.execQuery(
+        `SELECT * FROM myrecipes WHERE recipe_id = '${recipe_id}'`
+      )
+    )[0];
+    return { recipeInformataion: recipeInformataion, fromDB: true };
+  } else {
+    const recipeInformataion = await axios.get(
+      `${api_domain}/${recipe_id}/information`,
+      {
+        params: {
+          includeNutrition: false,
+          apiKey: process.env.spooncular_apiKey,
+        },
       }
+    );
+    return { recipeInformataion: recipeInformataion, fromDB: false };
+  }
 }
 
-
-async function getRecipeInformation(recipe_id, FromDB = false) {
-    if(FromDB)
-    {
-        const recipeInformataion = (
-            await DButils.execQuery(
-              `SELECT * FROM myrecipes WHERE recipe_id = '${recipe_id}'`
-            )
-          )[0];
-          return recipeInformataion;
-    }
-    else
-    {
-        return await axios.get(`${api_domain}/${recipe_id}/information`, {
-            params: {
-                includeNutrition: false,
-                apiKey: process.env.spooncular_apiKey
-            }
-        });
-    }
-
-}
-
-async function getRecipeDetails(recipe_id, fromDB = false) {
-    let recipe_info = await getRecipeInformation(recipe_id, fromDB);
-    if(fromDB)
-    {        
-        let { recipe_id, title, ready_in_minutes, image, vegan, vegetarian, is_gluten_free } = recipe_info;
-        return {
-            id: recipe_id,
-            title: title,
-            readyInMinutes: ready_in_minutes,
-            image: image,
-            popularity: 0,
-            vegan: Boolean(vegan),
-            vegetarian: Boolean(vegetarian),
-            glutenFree: Boolean(is_gluten_free),
-        }
-    }    
-    else
-    {
-        let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
-        return {
-            id: id,
-            title: title,
-            readyInMinutes: readyInMinutes,
-            image: image,
-            popularity: aggregateLikes,
-            vegan: vegan,
-            vegetarian: vegetarian,
-            glutenFree: glutenFree,
-            
-        }
-    }
-
+async function getRecipeDetails(recipe_id, is_search = false) {
+  let recipe_info = await getRecipeInformation(recipe_id);
+  const recipeInformataion = recipe_info.recipeInformataion;
+  const fromDB = recipe_info.fromDB;
+  if (fromDB && !is_search) {
+    let {
+      recipe_id,
+      title,
+      ready_in_minutes,
+      image,
+      vegan,
+      vegetarian,
+      is_gluten_free,
+    } = recipeInformataion;
+    return {
+      id: recipe_id,
+      title: title,
+      readyInMinutes: ready_in_minutes,
+      image: image,
+      popularity: 0,
+      vegan: Boolean(vegan),
+      vegetarian: Boolean(vegetarian),
+      glutenFree: Boolean(is_gluten_free),
+    };
+  } else {
+    let {
+      id,
+      title,
+      readyInMinutes,
+      image,
+      aggregateLikes,
+      vegan,
+      vegetarian,
+      glutenFree,
+    } = recipeInformataion.data;
+    return {
+      id: id,
+      title: title,
+      readyInMinutes: readyInMinutes,
+      image: image,
+      popularity: aggregateLikes,
+      vegan: vegan,
+      vegetarian: vegetarian,
+      glutenFree: glutenFree,
+    };
+  }
 }
 
 async function getRecipeFullInformation(recipe_id, fromDB) {
-    if (fromDB)
-    {
-        const recipe_informataion = (
-            await DButils.execQuery(
-              `SELECT * FROM myrecipes WHERE recipe_id = '${recipe_id}'`
-            )
-          )[0];
-        const recipe_ingredients = (
-            await DButils.execQuery(
-                `SELECT * FROM ingredients WHERE recipe_id = '${recipe_id}'`
-            )
-        );
-            const recipe_instructions = (
-            await DButils.execQuery(
-                `SELECT * FROM instructions WHERE recipe_id = '${recipe_id}' ORDER BY instruction_order`
-            )
-        );
+  if (fromDB) {
+    const recipe_informataion = (
+      await DButils.execQuery(
+        `SELECT * FROM myrecipes WHERE recipe_id = '${recipe_id}'`
+      )
+    )[0];
+    const recipe_ingredients = await DButils.execQuery(
+      `SELECT * FROM ingredients WHERE recipe_id = '${recipe_id}'`
+    );
+    const recipe_instructions = await DButils.execQuery(
+      `SELECT * FROM instructions WHERE recipe_id = '${recipe_id}' ORDER BY instruction_order`
+    );
 
-        const recipe = { recipe_informataion: recipe_informataion, 
-        recipe_ingredients: recipe_ingredients, 
-        recipe_instructions: recipe_instructions }
+    const recipe = {
+      recipe_informataion: recipe_informataion,
+      recipe_ingredients: recipe_ingredients,
+      recipe_instructions: recipe_instructions,
+    };
 
-        return recipe;
-    }
-    else
-    {
-        return await axios.get(`${api_domain}/${recipe_id}/information`, {
-            params: {
-                includeNutrition: false,
-                apiKey: process.env.spooncular_apiKey
-            }
-        });
-    }
-   
+    return recipe;
+  } else {
+    return await axios.get(`${api_domain}/${recipe_id}/information`, {
+      params: {
+        includeNutrition: false,
+        apiKey: process.env.spooncular_apiKey,
+      },
+    });
+  }
 }
 
 async function getRecipeFullDetails(recipe_id) {
-    const checkIfFromDB = await DButils.execQuery(`SELECT 1 FROM MyRecipes WHERE recipe_id = '${recipe_id}'`);
-    if (checkIfFromDB.length == 0)
-    {
-        let recipe = await getRecipeFullInformation(recipe_id);  
-        let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, instructions, extendedIngredients } = recipe.data;
-        return {
-            id: id,
-            title: title,
-            readyInMinutes: readyInMinutes,
-            image: image,
-            popularity: aggregateLikes,
-            vegan: vegan,
-            vegetarian: vegetarian,
-            glutenFree: glutenFree,
-            instructions: instructions,
-            extendedIngredients: extendedIngredients
-        };
-    }    
-    else
-    {
-        let recipe = await getRecipeFullInformation(recipe_id, true);        
-        let { recipe_id: db_recipe_id, title, ready_in_minutes, image, vegan, vegetarian, is_gluten_free } = recipe.recipe_informataion;
-        let ingredients = [];
-        for(const ingredient of recipe.recipe_ingredients)
-        {
-            let { name,  quantity, unit } = ingredient;
-            ingredients.push({ originalName: name,  amount: quantity, unit: unit });
-        }
-        let instructions = [];
-        for(const instruction of recipe.recipe_instructions)
-        {
-            let { instruction_text } = instruction;
-            instructions.push(instruction_text);
-        }
-        return {
-            id: db_recipe_id,
-            title: title,
-            readyInMinutes: ready_in_minutes,
-            image: image,
-            popularity: 0,
-            vegan: Boolean(vegan),
-            vegetarian: Boolean(vegetarian),
-            glutenFree: Boolean(is_gluten_free),
-            instructions: instructions,
-            extendedIngredients: ingredients
-        };
+  const checkIfFromDB = await DButils.execQuery(
+    `SELECT 1 FROM MyRecipes WHERE recipe_id = '${recipe_id}'`
+  );
+  if (checkIfFromDB.length == 0) {
+    let recipe = await getRecipeFullInformation(recipe_id);
+    let {
+      id,
+      title,
+      readyInMinutes,
+      image,
+      aggregateLikes,
+      vegan,
+      vegetarian,
+      glutenFree,
+      instructions,
+      extendedIngredients,
+    } = recipe.data;
+    return {
+      id: id,
+      title: title,
+      readyInMinutes: readyInMinutes,
+      image: image,
+      popularity: aggregateLikes,
+      vegan: vegan,
+      vegetarian: vegetarian,
+      glutenFree: glutenFree,
+      instructions: instructions,
+      extendedIngredients: extendedIngredients,
+    };
+  } else {
+    let recipe = await getRecipeFullInformation(recipe_id, true);
+    let {
+      recipe_id: db_recipe_id,
+      title,
+      ready_in_minutes,
+      image,
+      vegan,
+      vegetarian,
+      is_gluten_free,
+    } = recipe.recipe_informataion;
+    let ingredients = [];
+    for (const ingredient of recipe.recipe_ingredients) {
+      let { name, quantity, unit } = ingredient;
+      ingredients.push({ originalName: name, amount: quantity, unit: unit });
     }
+    let instructions = [];
+    for (const instruction of recipe.recipe_instructions) {
+      let { instruction_text } = instruction;
+      instructions.push(instruction_text);
+    }
+    return {
+      id: db_recipe_id,
+      title: title,
+      readyInMinutes: ready_in_minutes,
+      image: image,
+      popularity: 0,
+      vegan: Boolean(vegan),
+      vegetarian: Boolean(vegetarian),
+      glutenFree: Boolean(is_gluten_free),
+      instructions: instructions,
+      extendedIngredients: ingredients,
+    };
+  }
 }
-
-
 
 async function searchRecipe(recipeName, cuisine, diet, intolerance, number) {
-    const response = await axios.get(`${api_domain}/complexSearch`, {
-        params: {
-            query: recipeName,
-            cuisine: cuisine,
-            diet: diet,
-            intolerances: intolerance,
-            number: number,
-            apiKey: process.env.spooncular_apiKey
-        }
-    });
+  const response = await axios.get(`${api_domain}/complexSearch`, {
+    params: {
+      query: recipeName,
+      cuisine: cuisine,
+      diet: diet,
+      intolerances: intolerance,
+      number: number,
+      apiKey: process.env.spooncular_apiKey,
+    },
+  });
 
-    return getRecipesPreview(response.data.results.map((element) => element.id));
+  return getRecipesPreview(
+    response.data.results.map((element) => element.id),
+    true
+  );
 }
 
-
-async function getRecipesPreview(recipes_id_array) {
+async function getRecipesPreview(recipes_id_array, is_search = false) {
   const recipesDetailsArray = [];
   const recipeDetailsPromises = recipes_id_array.map(async (recipe_id) => {
-      const recipeDetails = await getRecipeDetails(recipe_id);
-      return recipeDetails;
+    const recipeDetails = await getRecipeDetails(recipe_id, is_search);
+    return recipeDetails;
   });
 
   const resolvedRecipesDetails = await Promise.all(recipeDetailsPromises);
@@ -212,7 +241,4 @@ exports.getRecipeDetails = getRecipeDetails;
 exports.getRecipeFullDetails = getRecipeFullDetails;
 exports.getRandomRecipes = getRandomRecipes;
 exports.searchRecipe = searchRecipe;
-exports.getRecipesPreview =getRecipesPreview;
-
-
-
+exports.getRecipesPreview = getRecipesPreview;
